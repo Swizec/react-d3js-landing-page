674aea9ae0e96233a84e2efd9849a8613770180c
Swizec Teller <swizec@swizec.com>
2016-03-22 23:41:24 -0700

    Start using React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <p>Hello world</p>
+        );
+    }
+}
+
+export default App;

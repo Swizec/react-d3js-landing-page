339a01c073ae1ccfa540f6d6675b128758807ad0
Swizec Teller <swizec@swizec.com>
2016-03-28 02:56:35 -0700

    Prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -33,6 +33,11 @@ module.exports = {
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
     },
+    {
+      test: /\.js|\.jsx?/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'node_modules/react-examples-gallery')
+    },
       {
         test: /\.less$/,
         loader: "style!css!less"

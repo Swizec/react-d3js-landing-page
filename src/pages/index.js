8b8a516a322850875effd954a42031bd60013047
CraftAtoms <creator@craftatoms.com>
2018-08-26 14:10:53 -0400

    section2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import Hero from '../components/Hero/Hero';
+import Section2 from '../components/Section2';
 
 const IndexPage = () => (
   <Layout>
     <Hero/>
+    <Section2/>
   </Layout>
 )
 

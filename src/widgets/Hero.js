51ed8d24a73f3c563c687854355005f717ec8c87
CraftAtoms <creator@craftatoms.com>
2018-08-31 14:41:13 -0400

    Hero Styles

--- src/widgets/Hero.js
+++ src/widgets/Hero.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react'
 import styled from "styled-components";
 import webappbanner from '../images/web-app-banner.png'
 import logo from '../images/readymade_logo.png'
+import wallpaper from '../images/header_bg.jpg'
+
 import './widget.css'
 
 const Wrapper = styled.div`
+background-image: url(${wallpaper});
 text-align: center;
+
+h1 {
+  color: #fff;
+}
+h4 {
+  color: #fff;
+  font-weight: 200;
+  line-height: 1.1;
+}
+@media (max-width: 640px) {
+  h1 {
+  font-size: 30px;
+  }
+  h4 {
+    font-size: 18px;
+  }
+  .image {
+    height: 300px;
+  }
+}
 `
 
 export default class Hero extends Component {
@@ -27,7 +50,7 @@ export default class Hero extends Component {
             
             <div class="row">
                 <div class="col-md-12">
-                <img src={webappbanner} alt="Web App Banner" class="img-responsive center-block"/>
+                <img src={webappbanner} alt="Web App Banner" class="image"/>
                 </div>
             </div>
             

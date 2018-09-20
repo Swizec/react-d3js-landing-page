6566a49d93bcaa43102235879b6bbf7c5771d0d9
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-09-20 16:37:57 -0400

    Update social sharing tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import logo from '../images/logo.png'
+import product from '../images/Real-Space-Gray.png'
 
 import './layout.css'
 
@@ -13,6 +13,8 @@ const Layout = ({ children, data }) => (
         site {
           siteMetadata {
             title
+            description
+            url
           }
         }
       }
@@ -42,13 +44,19 @@ const Layout = ({ children, data }) => (
           />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
+          {/* TODO change `og:type` to `books.book` and add
+          property `books:isbn` when publisher releases isbn */}
+          <meta property="og:type" content={'product'} />
           <meta property="og:url" content={data.site.siteMetadata.url} />
           <meta property="og:title" content={data.site.siteMetadata.title} />
-          <meta property="og:image" content={logo} />
+          <meta property="og:image" content={product} />
+          <meta property="og:image:width" content={1154} />
+          <meta property="og:image:height" content={1600} />
           <meta
             property="og:description"
             content={data.site.siteMetadata.description}
           />
+          {/* TODO add `fb:app_id` property */}
 
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@swizec" />
@@ -58,7 +66,7 @@ const Layout = ({ children, data }) => (
             name="twitter:description"
             content={data.site.siteMetadata.description}
           />
-          <meta name="twitter:image" content={logo} />
+          <meta name="twitter:image" content={product} />
         </Helmet>
 
         {children}

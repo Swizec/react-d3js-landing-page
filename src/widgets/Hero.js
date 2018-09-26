7139e4c0ff8390edf4d89faa30b0bcc9fd0d098c
CraftAtoms <creator@craftatoms.com>
2018-09-25 20:26:25 -0400

    Hero Responsive

--- src/widgets/Hero.js
+++ src/widgets/Hero.js
@@ -2,22 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  margin: 0 0;
-  height: 600px;
+  margin: 0 auto;
+  height: 100%;
   text-align: center;
   color: #fff;
   text-shadow: 0 1px 0 rgba(0, 0, 0, 0.1);
   background-color: #424ea4;
+  border-top: 0.4rem solid #ff871c;
 `
 
 const WrapperGroup = styled.div`
-  margin: 0 5rem;
+  margin: 0 auto;
   padding: 5rem 5rem;
   display: grid;
   align-items: center;
   justify-items: center;
   grid-template-columns: 1fr 1fr;
   grid-template-areas: 'ContentA ContentB';
+  @media (max-width: 940px) {
+    padding: 5rem 2rem;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      'ContentA'
+      'ContentB';
+  }
 `
 const WrapperTopSub = styled.div`
   margin: 1rem 0;
@@ -28,6 +36,9 @@ const WrapperTopSub = styled.div`
 const WrapperHeader = styled.div`
   font-size: 3rem;
   font-weight: 600;
+  @media (max-width: 940px) {
+    font-size: 2.5rem;
+  }
 `
 const WrapperSubtitle = styled.div`
   margin: 1rem 0;
@@ -35,10 +46,15 @@ const WrapperSubtitle = styled.div`
   line-height: 1.5;
   font-weight: 200;
 `
+const WrapperEmailText = styled.div`
+  padding: 1rem 0;
+  font-size: 1rem;
+  color: #e5e8ec;
+  line-height: 1.5;
+  font-weight: 600;
+`
 const WrapperEmailInput = styled.div`
   input {
-    margin-left: 10px;
-    overflow: hidden;
     display: block;
     width: 100%;
     height: 36px;
@@ -49,24 +65,17 @@ const WrapperEmailInput = styled.div`
     background-color: #fff;
     background-image: none;
     border: 1px solid #cfd3cc;
-    border-radius: 2px;
-    -webkit-box-shadow: none;
-    box-shadow: none;
-    -webkit-transition: border-color ease-in-out 0.15s,
-      box-shadow ease-in-out 0.15s;
-    transition: border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s;
+    border-radius: 0.5rem;
+    -webkit-box-shadow: 0 15px 35px 0 rgba(42, 51, 83, 0.12),
+      0 5px 15px rgba(0, 0, 0, 0.06);
+    box-shadow: 0 15px 35px 0 rgba(42, 51, 83, 0.12),
+      0 5px 15px rgba(0, 0, 0, 0.06);
     margin: 0;
   }
 `
-const WrapperEmailText = styled.div`
-  padding: 1rem 0;
-  font-size: 1rem;
-  color: #e5e8ec;
-  line-height: 1.5;
-  font-weight: 600;
-`
 const WrapperEmailSubmit = styled.div`
   input {
+    margin-left: 10px;
     float: right;
     display: block;
     padding: 6px 14px;
@@ -86,6 +95,7 @@ const WrapperEmailSubmit = styled.div`
     background-clip: padding-box;
     border: 1px solid transparent;
     border-radius: 2px;
+    text-transform: uppercase;
     background: #ff871c;
     color: #fff;
   }
@@ -94,6 +104,12 @@ const WrapperEmailSubmit = styled.div`
 const WrapperImage = styled.image`
   img {
     height: 400px;
+  }
+  @media (max-width: 940px) {
+    img {
+      margin: 3rem 0 0;
+      height: 300px;
+    }
   }
 `
 const Content2 = styled.div`
@@ -112,7 +128,9 @@ const Hero = props => (
         <WrapperHeader>{props.header}</WrapperHeader>
         <WrapperSubtitle>{props.subtitle}</WrapperSubtitle>
         <WrapperEmailText>{props.emailtext}</WrapperEmailText>
-        <WrapperEmailInput>{props.input}</WrapperEmailInput>
+        <WrapperEmailInput className="heroinput">
+          {props.input}
+        </WrapperEmailInput>
         <WrapperEmailSubmit>{props.submit}</WrapperEmailSubmit>
       </Content1>
 
